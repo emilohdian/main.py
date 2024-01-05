e98fdf64f604a4f16db9545b2c70cc594f24aba6
emilohdian <147985410+emilohdian@users.noreply.github.com>
2024-01-05 16:50:11 +0200

    scan.py

--- scan.py
+++ scan.py
@@ -6,19 +6,6 @@ png_files = list()
 jpg_files = list()
 txt_files = list()
 docx_files = list()
-svg_files = list()
-avi_files = list()
-mp4_files = list()
-mov_files = list()
-mkv_files = list()
-doc_files = list()
-pdf_files = list()
-xlsx_files = list()
-pptx_files = list()
-mp3_files = list()
-ogg_files = list()
-wav_files = list()
-amr_files = list()
 folders = list()
 archives = list()
 others = list()
@@ -32,19 +19,6 @@ registered_extensions = {
     'TXT': txt_files,
     'DOCX': docx_files,
     'ZIP': archives,
-    'SVG': svg_files,
-    'AVI': avi_files,
-    'MP4': mp4_files,
-    'MOV': mov_files,
-    'MKV': mkv_files,
-    'DOC': doc_files,
-    'PDF': pdf_files,
-    'XLSX': xlsx_files,
-    'PPTX': pptx_files,
-    'MP3': mp3_files,
-    'OGG': ogg_files,
-    'WAV': wav_files,
-    'AMR': amr_files
 }
 
 def get_extensions(file_name):
@@ -84,19 +58,6 @@ if __name__ == '__main__':
     print(f"png: {png_files}")
     print(f"txt: {txt_files}")
     print(f"docx: {docx_files}")
-    print(f"svg: {svg_files}")
-    print(f"avi: {avi_files}")
-    print(f"mp4: {mp4_files}")
-    print(f"mov: {mov_files}")
-    print(f"mkv: {mkv_files}")
-    print(f"doc: {doc_files}")
-    print(f"pdf: {pdf_files}")
-    print(f"xlsx: {xlsx_files}")
-    print(f"pptx: {pptx_files}")
-    print(f"mp3: {mp3_files}")
-    print(f"ogg: {ogg_files}")
-    print(f"wav: {wav_files}")
-    print(f"amr: {amr_files}")
     print(f"archive: {archives}")
     print(f"unkown: {others}")
     print(f"All extensions: {extensions}")
