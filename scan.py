8707a5fd81e6e0ef80eb50c1d00e2391da05c0a3
emilohdian <147985410+emilohdian@users.noreply.github.com>
2024-02-09 10:48:02 +0200

    scan.py

--- scan.py
+++ scan.py
@@ -6,6 +6,21 @@ png_files = list()
 jpg_files = list()
 txt_files = list()
 docx_files = list()
+svg_files = list()
+avi_files = list()
+mp4_files = list()
+mov_files = list()
+mkv_files = list()
+doc_files = list()
+pdf_files = list()
+xlsx_files = list()
+pptx_files = list()
+mp3_files = list()
+ogg_files = list()
+wav_files = list()
+amr_files = list()
+tar_files = list()
+gz_files = list()
 folders = list()
 archives = list()
 others = list()
@@ -19,6 +34,21 @@ registered_extensions = {
     'TXT': txt_files,
     'DOCX': docx_files,
     'ZIP': archives,
+    'SVG': svg_files,
+    'AVI': avi_files,
+    'MP4': mp4_files,
+    'MOV': mov_files,
+    'MKV': mkv_files,
+    'DOC': doc_files,
+    'PDF': pdf_files,
+    'XLSX': xlsx_files,
+    'PPTX': pptx_files,
+    'MP3': mp3_files,
+    'OGG': ogg_files,
+    'WAV': wav_files,
+    'AMR': amr_files,
+    'TAR': tar_files,
+    'GZ': gz_files
 }
 
 def get_extensions(file_name):
@@ -27,7 +57,7 @@ def get_extensions(file_name):
 def scan(folder):
     for item in folder.iterdir():
         if item.is_dir():
-            if item.name not in ('JPEG', 'PNG', 'JPG', 'TXT', 'DOCX', 'ARCHIVE', 'OTHER'):
+            if item.name not in ('JPEG', 'PNG', 'JPG', 'TXT', 'DOCX', 'ARCHIVE', 'OTHER', 'ZIP','SVG', 'AVI', 'MP4', 'MOV', 'MKV', 'DOC', 'PDF', 'XLSX', 'PPTX', 'MP3', 'OGG', 'WAV', 'AMR', 'TAR', 'GZ'):
                 folders.append(item)
                 scan(item)
             continue
@@ -46,18 +76,32 @@ def scan(folder):
                 others.append(new_name)
 
 if __name__ == '__main__':
-    path = sys.argv[1]
-    print(f"Start in {path}")
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+        print(f"Start in {path}")
 
-    folder = Path(path)
+        folder = Path(path)
 
-    scan(folder)
+        scan(folder)
 
     print(f"jpeg: {jpeg_files}")
     print(f"jpg: {jpg_files}")
     print(f"png: {png_files}")
     print(f"txt: {txt_files}")
     print(f"docx: {docx_files}")
+    print(f"svg: {svg_files}")
+    print(f"avi: {avi_files}")
+    print(f"mp4: {mp4_files}")
+    print(f"mov: {mov_files}")
+    print(f"mkv: {mkv_files}")
+    print(f"doc: {doc_files}")
+    print(f"pdf: {pdf_files}")
+    print(f"xlsx: {xlsx_files}")
+    print(f"pptx: {pptx_files}")
+    print(f"mp3: {mp3_files}")
+    print(f"ogg: {ogg_files}")
+    print(f"wav: {wav_files}")
+    print(f"amr: {amr_files}")
     print(f"archive: {archives}")
     print(f"unkown: {others}")
     print(f"All extensions: {extensions}")
